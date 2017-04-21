f85ca928d1ac603ebffe00b665bdf544729c9ba6
LeCodeBae <billivank_96@yahoo.co.id>
2017-04-21 11:54:49 +0700

    tambah cat update, tampilan dibersihin

--- routes/categories.js
+++ routes/categories.js
@@ -12,5 +12,34 @@ router.post('/add', function(req, res){
     res.redirect('/')
   })
 })
-
+router.get('/delete', function(req, res){
+  db.Category.findAll({order: '"id" ASC'}).then((categories)=>{
+    res.render('choosecategory.ejs', {title: "Delete Category", action: "delete", categories: categories})
+  })
+})
+router.post('/delete', function(req,res){
+  db.Category.destroy({where: {id: req.body.id}})
+  .then(()=>{
+    res.redirect('/')
+  })
+})
+router.get('/update', function(req,res){
+  db.Category.findAll({order: '"id" ASC'}).then((categories)=>{
+    res.render('choosecategory.ejs', {title: "Update Category", action: "update", categories: categories})
+  })
+})
+router.post('/update', function(req, res){
+  db.Category.findOne({where: {id: req.body.id}}).then((category)=>{
+    res.render('updatecategory.ejs', {category: category})
+  })
+})
+router.post('/update/:id', function(req, res){
+  db.Category.findOne({where: {id: req.params.id}}).then((category)=>{
+    category.updateAttributes({
+      name: req.body.name
+    }).then(()=>{
+      res.redirect('/')
+    })
+  })
+})
 module.exports = router;
